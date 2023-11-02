cc62607167229ce0cde7d24df7a652445c8fb3f6
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-11-02 15:56:13 -0600

    feat: added app footer description

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const canvas = document.getElementById("main-canvas");
-const context = canvas.getContext("2d");
+/*
+    * Este es el inicio de mi demencia, hasta donde mis ganas de aprender
+    * me permitiran ir, que horizontes me seran posible explorar
+    */
+
+var canvas = document.getElementById("main-canvas");
+var context = canvas.getContext("2d");
 
 
 context.fillStyle = "red";
-context.fillRect(10, 10, 1, 1);
+// context.fillRect(10, 10, 1, 1);
+
+
+function drawPixel(x, y) {
+    context.fillRect(x, y, 1, 1);
+};
+
+
+for (let i=10; i <= 200; i++) {
+    drawPixel(i, 10);
+};
 
 
 // function drawCircle() {
