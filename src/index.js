10a25dd26d711c1f5b55cb4a2124c3b4827b39c0
eescobedopc <eescobedopc@corporativosade.com.mx>
2023-11-02 09:37:33 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById("main-canvas");
+const context = canvas.getContext("2d");
+
+
+context.fillStyle = "red";
+context.fillRect(10, 10, 1, 1);
+
+
+// function drawCircle() {
+//     let pi = 3.1416;
+//     let d = 50;
+// 
+//     let circunferencia = pi * d;
+// };
